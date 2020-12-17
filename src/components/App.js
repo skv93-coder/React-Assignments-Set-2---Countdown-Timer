dac54b01942c8f424fa911be99a0718403296312
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-12-18 01:21:53 +0530

    ch

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,45 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  const [value, setvalue] = useState("");
+  const [curTime, setcurrTime] = useState("");
+  const handle = (ev) => {
+    setvalue(ev.target.value);
+    const input = Number(ev.target.value, 10);
+    console.log(input);
+    if (input) {
+      setcurrTime(input);
 
+      // console.log(input);
+    } else {
+      setcurrTime(0);
+    }
+  };
+  const tick = () => {
+    if (curTime) {
+      setcurrTime(curTime - 1);
+      //   console.log(1);
+    }
+  };
+  useEffect(() => {
+    setTimeout(tick, 1000);
+    return () => {
+      clearTimeout();
+    };
+  }, [curTime]);
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount" value={value} onChange={handle} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{curTime}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
